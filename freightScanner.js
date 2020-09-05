54c6f432d714c6d4146511d2eb00708fb5aabdc7
Danyil Gregul <daniel.gregul95@gmail.com>
2020-09-05 14:24:16 +0200

    Freight Scanner Solved

--- freightScanner.js
+++ freightScanner.js
@@ -1,24 +1,14 @@
 function scan(array) {
     let contrabandCount = 0;
+    
+    array.forEach(item => {
+        if (item == 'contraband') {
+            contrabandCount++;
+        }
+    });
 
-    function check(value) {
-        let counter = 0;
-        if (value = 'contraband') {
-            counter + 1
-        };
-        return counter;
-    };
-
-    array.forEach(element => check(element));
-
-
-    contrabandCount = check()
     return contrabandCount;
 }
-
-["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
-    console.log((`${item} is at index ${index} in ${array}`));
-  });
 
 const numItems = scan(['dog', 'contraband', 'cat', 'zippers', 'contraband']);
 console.log('Number of "contraband": ' + numItems);
